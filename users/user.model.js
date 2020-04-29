cbeae8974f3e23e384f1bbb7896ab1f80f9003e8
Okekunle Oluwatobiloba <okekunletobi@gmail.com>
2020-04-29 19:40:36 +0100

    first commit

---
+++ users/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    username: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    active: { type: Boolean, default: true, select: false },
+    role: { type: String, enum: ["student", "tutor"], default: "student" },
+    createdDate: { type: Date, default: Date.now }
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.hash;
+    }
+});
+
+module.exports = mongoose.model('User', schema);
