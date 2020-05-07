406e6d037b309d7015dd7616ee5cc9aad4dc5ad5
Okekunle Oluwatobiloba <okekunletobi@gmail.com>
2020-05-07 19:26:54 +0100

    tired

--- users/user.model.js
+++ users/user.model.js
@@ -7,7 +7,6 @@ const schema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
-    active: { type: Boolean, default: true, select: false },
     role: { type: String, enum: ["student", "tutor"], default: "student" },
     createdDate: { type: Date, default: Date.now }
 });
--- users/users.controller.js
+++ users/users.controller.js
@@ -21,7 +21,7 @@ function authenticate(req, res, next) {
 
 function register(req, res, next) {
     userService.create(req.body)
-        .then(() => res.json({}))
+        .then(() => res.json({ message: "Registration Successful" }))
         .catch(err => next(err));
 }
 
